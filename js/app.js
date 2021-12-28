82d2235e38623260c929e2a785be8f6b5b773e8e
MiladNz <milad.n91@gmail.com>
2021-12-28 20:52:55 +0330

    first-final

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const toggler = document.querySelector(".nav__toggler");
 const navbar = document.querySelector(".nav");
-toggler.addEventListener("click" , (e) => {
-    navbar.classList.toggle("nav__expanded");
+toggler.addEventListener("click", (e) => {
+  navbar.classList.toggle("nav__expanded");
 });
 
 const tabs = document.querySelectorAll(".tab");
